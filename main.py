ab783834c9263a2a2979863ec93580df82637f2e
Jonathan Dyer <jonathandyer4153@gmail.com>
2025-07-19 23:51:45 -0500

    Bug Fix

--- main.py
+++ main.py
@@ -20,11 +20,9 @@ items = {
     6: '1 Strange Dagger'
 }
 
-#Function to return a string of the name of the room that corresponds to the number associated with the 'loc' variable
+#Simple function that takes in an integer and returns the name associated with it
 def get_room(current):
-    for i in range(len(rooms)):
-        if current == rooms[i]:
-            return rooms[i]
+    return rooms.get(current)
 
 #Function that prompts the player to decide which direction to go based on where they are currently
 def travel(room):
@@ -34,6 +32,21 @@ def travel(room):
         choice = int(input(f'You are at {room} you can go North or South\nType [1] to go North\nType [2] to go South\n'))
     return choice
 
-#First working line of code!
-#This gets user input and stores it as a number value inside of 'choice'
-choice = travel(rooms[0])
+#Welcom/'Main Menu'
+print('You wake up and look out your window.  The witches hut is glowing bright colors and you hear screams.')
+print('There must be something you can do!')
+print('You must visit your fellow viliagers and collect items to seige the witches hut!')
+next = int(input('Type [1] to play!'))
+
+#Game starts!
+#Room is set by the starting point '0' and choice is set by the travel function
+#Finally we update the room variable based on which direction the player chooses to go
+if next == 1:
+    room = get_room(0)
+    choice = travel(room)
+    if choice == 1:
+        print('Heading North!\n\n\n')
+        room = get_room(1)
+    elif choice == 2:
+        print('Heading West!\n\n\n')
+        room = get_room(3)
